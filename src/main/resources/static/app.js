eded0a27decd2680a78aae7457861cce3ec81789
Daniel Andres Ayala <daniel.ayala@mail.escuelaing.edu.co>
2016-10-12 17:13:22 -0500

    adelanto lab7

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,53 @@
+var stompClient = null;
+
+function connect() {
+    var socket = new SockJS('/stompendpoint');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        console.log('Connected: ' + frame);
+        
+        stompClient.subscribe('/topic/newpoint', function (data) {
+        var theObject =JSON.parse(data.body);
+        
+            alert(theObject.x + "  " + theObject.y);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient != null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendPoint(){
+    var coorx = $("#x").val();
+    var coory = $("#y").val();
+    stompClient.send("/topic/newpoint", {}, JSON.stringify({x: coorx, y: coory}));
+}
+
+$(document).ready(
+        function () {
+            connect();
+            console.info('connecting to websockets');
+
+        }
+             
+);
+function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+          x: evt.clientX - rect.left,
+          y: evt.clientY - rect.top
+        };
+      }
+      var canvas = document.getElementById('myCanvas');
+      //var context = canvas.getContext('2d');
+
+      canvas.addEventListener('mousedown', function(evt) {
+        var mousePos = getMousePos(canvas, evt);
+        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+        writeMessage(canvas, message);
+      }, false);
