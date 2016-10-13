b8c039597e3da536de03a77474d94ac33b11ee90
Santiago Chisco <david.chisco@mail.escuelaing.edu.co>
2016-10-13 11:21:03 -0500

    Parte 2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -9,7 +9,12 @@ function connect() {
         stompClient.subscribe('/topic/newpoint', function (data) {
         var theObject =JSON.parse(data.body);
         
-            alert(theObject.x + "  " + theObject.y);
+            
+            var canvas = document.getElementById('myCanvas');
+            var context = canvas.getContext('2d');
+            context.beginPath();
+            context.arc(theObject.x,theObject.y,2,2,3*Math.PI);
+            context.stroke();
         });
     });
 }
@@ -33,21 +38,24 @@ $(document).ready(
             connect();
             console.info('connecting to websockets');
 
-        }
-             
-);
-function getMousePos(canvas, evt) {
-        var rect = canvas.getBoundingClientRect();
-        return {
-          x: evt.clientX - rect.left,
-          y: evt.clientY - rect.top
-        };
-      }
-      var canvas = document.getElementById('myCanvas');
-      //var context = canvas.getContext('2d');
+            function getMousePos(canvas, evt) {
+                var rect = canvas.getBoundingClientRect();
+                return {
+                    x: evt.clientX - rect.left,
+                    y: evt.clientY - rect.top
+                };
+            }
+            var canvas = document.getElementById('myCanvas');
+            var context = canvas.getContext('2d');
 
-      canvas.addEventListener('mousedown', function(evt) {
-        var mousePos = getMousePos(canvas, evt);
-        var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
-        writeMessage(canvas, message);
-      }, false);
+            canvas.addEventListener('mousedown', function (evt) {
+                var mousePos = getMousePos(canvas, evt);
+                var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+                stompClient.send("/topic/newpoint", {}, JSON.stringify({x: mousePos.x, y: mousePos.y}));
+                //writeMessage(canvas, message);
+            }, false);
+
+        }
+
+);
+
